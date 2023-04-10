040d6b0990f1ed02cba222b691c8d41c4f6fc3f0
magga <adb.dao@gmail.com>
2023-04-10 16:25:00 +0800

    update

--- sw_my.js
+++ sw_my.js
@@ -1,29 +1,72 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw_my.js')
-    .then(function (registration) {
-      alert('安裝成功')
-    }).catch(function (err) {
-      alert('安裝失敗，請重新安裝')
-    })
-}
+// "start_url": "./?source=pwa",
 
-const cacheName = 'cache';
+// window.addEventListener('load', function () {
+//   var btnSave = document.getElementById('addPWA')
+//   var savedPrompt
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sw_my.js')
+//       .then(function (registration) {
+//         console.log('ServiceWorker registration successful with scope:', registration.scope)
+//         // alert('安裝成功')
+//       }).catch(function (err) {
+//         console.log('ServiceWorker registration failed:', err)
+//         // alert('安裝失敗，請重新安裝')
+//       })
+//   }
+
+//   window.addEventListener('beforeinstallprompt', function (e) {
+//     e.preventDefault()
+//     btnSave.removeAttribute('disabled')
+//     savedPrompt = e
+//     return false
+//   })
+
+//   btnSave.addEventListener('click', function () {
+//     if (savedPrompt !== undefined) {
+//       savedPrompt.prompt()
+
+//       savedPrompt.userChoice.then(function (result) {
+
+//         if (result.outcome == 'dismissed') {
+//           //   alert('使用者關閉主營幕')
+//           console.log('User dismissed homescreen install')
+//         } else {
+//           // alert('使用者加入主營幕')
+//           console.log('User added to homescreen')
+//         }
+//         savedPrompt = null
+//       })
+//     }
+//   })
+
+// })
+// ========================
+
+const cacheName = 'helloWorld';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-        'index.html'
+        'index.html',
+        'sw_my.js',
+        'manifest.json',
+        'icons/virucana48.png',
+        'icons/virucana144.png',
+        'icons/virucana192.png',
+        'icons/virucana512.png'
       ]))
-  )})
+  )
+})
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
       .then(function (response) {
         if (response) {
           return response
         }
-        return fetch(e.request)
+        return fetch(event.request)
       }))
 });
